97eed831a0189e04b8e39c9a8ebb61c281832317
danx <320223@college.local>
2023-03-14 10:31:33 +0300

    comment

---
+++ ConsoleApp10/Library.cs
@@ -0,0 +1,15 @@
+﻿namespace laba_1
+{
+    public class Library
+    {
+        public Library(string name, string adress, int year)
+        {
+            Name = name;
+            Adress = adress;
+            Year = year;
+        }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace laba_1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Введите количество библиотек: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            ArrayList arrayList = new ArrayList(N);
+
+            for (int i = 0; i < N;)
+            {
+                try
+                {
+                    Console.Write("Название: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("Адрес: ");
+                    string adress = Console.ReadLine();
+
+                    Console.Write("Год открытия: ");
+                    int year = Convert.ToInt32(Console.ReadLine());
+
+                    Library Library = new Library(name, adress, year);
+                    arrayList.Add(Library);
+
+                    i++;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введенные данные были неверными");
+                }
+                Console.WriteLine();
+            }
+
+            string path = "ArrayList.txt";
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                foreach (Library word in arrayList)
+                {
+                    writer.WriteLine(word.Name);
+                    writer.WriteLine(word.Adress);
+                    writer.WriteLine(word.Year);
+                    writer.WriteLine();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
